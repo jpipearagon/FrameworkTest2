32cc65fe95603eacbbab755587b3b080e83b11ba
Felipe Aragon <felipe.aragon@rappi.com>
2020-04-05 20:57:20 -0500

    init framework

---
+++ FrameworkTest2/FrameworkTest2/FrameworkTest2.h
@@ -0,0 +1,19 @@
+//
+//  FrameworkTest2.h
+//  FrameworkTest2
+//
+//  Created by Felipe Aragon on 5/04/20.
+//  Copyright © 2020 BrangerBriz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FrameworkTest2.
+FOUNDATION_EXPORT double FrameworkTest2VersionNumber;
+
+//! Project version string for FrameworkTest2.
+FOUNDATION_EXPORT const unsigned char FrameworkTest2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkTest2/PublicHeader.h>
+
+
